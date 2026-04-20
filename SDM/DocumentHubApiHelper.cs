0f2a77417499b2225f6799a32d218055767b3b63
SKYLINE2\BenjaminHO <benjamin.hodzic@skyline.be>
2026-04-20 14:51:43 +0200

    Made the ApiHelper public and added public comments/docs

--- SDM/DocumentHubApiHelper.cs
+++ SDM/DocumentHubApiHelper.cs
@@ -4,12 +4,22 @@
     using Skyline.DataMiner.SDM;
     using Skyline.DataMiner.Solutions.DocumentHub.SDM.Models;
 
-    internal class DocumentHubApiHelper : IDocumentHubApiHelper
+    /// <summary>
+    /// Provides access to repositories and configuration data for the Document Hub API.
+    /// </summary>
+    /// <remarks>This helper class centralizes access to document categories, DOM sources, and SharePoint
+    /// configuration repositories.. It is intended to simplify data access patterns for
+    /// consumers of the Document Hub API.</remarks>
+    public class DocumentHubApiHelper : IDocumentHubApiHelper
     {
         private readonly IBulkRepository<DocumentCategory> _documentCategories;
         private readonly IRepository<DomSource> _domSources;
         private readonly IRepository<SharePointConfiguration> _sharePointConfigurations;
 
+        /// <summary>
+        /// Initializes a new instance of the DocumentHubApiHelper class using the specified connection.
+        /// </summary>
+        /// <param name="connection">The <see cref="IConnection"/> to use for data repository access.</param>
         public DocumentHubApiHelper(IConnection connection)
         {
             Connection = connection;
@@ -18,8 +28,14 @@
             _sharePointConfigurations = new SharePointConfigurationDomRepository(connection);
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public IConnection Connection { get; }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public IBulkRepository<DocumentCategory> DocumentCategories 
         {
             get
@@ -28,6 +44,9 @@
             }
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public IRepository<DomSource> DomSources 
         {
             get
@@ -36,6 +55,9 @@
             }
         }
 
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
         public IRepository<SharePointConfiguration> SharePointConfigurations
         {
             get
--- SDM/IDocumentHubApiHelper.cs
+++ SDM/IDocumentHubApiHelper.cs
@@ -3,16 +3,30 @@
     using Skyline.DataMiner.Net;
     using Skyline.DataMiner.SDM;
     using Skyline.DataMiner.Solutions.DocumentHub.SDM.Models;
-    using System;
 
-    internal interface IDocumentHubApiHelper
+    /// <summary>
+    /// Provides access to repositories for interacting with Document Hub data sources and configurations.
+    /// </summary>
+    public interface IDocumentHubApiHelper
     {
+        /// <summary>
+        /// Gets the underlying connection used for communication with the repositories.
+        /// </summary>
         IConnection Connection { get; }
 
+        /// <summary>
+        /// Gets the repository for <see cref="DocumentCategory"/> objects.
+        /// </summary>
         IBulkRepository<DocumentCategory> DocumentCategories { get; }
 
+        /// <summary>
+        /// Gets the repository for <see cref="DomSource"/> objects.
+        /// </summary>
         IRepository<DomSource> DomSources { get; }
 
+        /// <summary>
+        /// Gets the repository for <see cref="SharePointConfiguration"/> objects.
+        /// </summary>
         IRepository<SharePointConfiguration> SharePointConfigurations { get; }
     }
 }
