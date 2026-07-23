a0b573f61803696fcb73accf100549e516439e3e
Benjamin Hodžić <benjamin.hodzic@skyline.be>
2026-07-23 15:19:53 +0200

    Async compatibility with folder and remaining page queues

--- DevPack/API/StorageHandlers/Paging/SharePointPageData.cs
+++ DevPack/API/StorageHandlers/Paging/SharePointPageData.cs
@@ -1,43 +1,44 @@
 ﻿namespace Skyline.DataMiner.Solutions.DocumentHub.API.Paging
 {
-    using System.Collections.Generic;
-    using Microsoft.Graph.Models;
+	using System.Collections.Concurrent;
+	using System.Collections.Generic;
+	using Microsoft.Graph.Models;
 
-    /// <summary>
-    /// Paging context used when reading files from SharePoint storage.
-    /// </summary>
-    public class SharePointPageData : DocHubPageData
-    {
-        public SharePointPageData()
-        {
-        }
+	/// <summary>
+	/// Paging context used when reading files from SharePoint storage.
+	/// </summary>
+	public class SharePointPageData : DocHubPageData
+	{
+		public SharePointPageData()
+		{
+		}
 
-        /// <summary>
-        /// Buffer for DriveItems that did not fit into the current logical page.
-        /// </summary>
-        public Queue<DriveItem> PageRemainderBuffer { get; } = new Queue<DriveItem>();
+		/// <summary>
+		/// Buffer for DriveItems that did not fit into the current logical page.
+		/// </summary>
+		public ConcurrentQueue<DriveItem> PageRemainderBuffer { get; } = new ConcurrentQueue<DriveItem>();
 
-        /// <summary>
-        /// Identifiers of folders that still need to be traversed.
-        /// </summary>
-        public Queue<string> FolderQueue { get; internal set; } = new Queue<string>(new[] { "root" });
+		/// <summary>
+		/// Identifiers of folders that still need to be traversed.
+		/// </summary>
+		public ConcurrentQueue<string> FolderQueue { get; internal set; } = new ConcurrentQueue<string>(new[] { "root" });
 
-        /// <summary>
-        /// Continuation URL (@odata.nextLink) for the folder currently being paged.
-        /// Null when a new folder traversal must be started.
-        /// </summary>
-        public string NextPageLink { get; internal set; }
+		/// <summary>
+		/// Continuation URL (@odata.nextLink) for the folder currently being paged.
+		/// Null when a new folder traversal must be started.
+		/// </summary>
+		public string NextPageLink { get; internal set; }
 
-        /// <summary>
-        /// Id of the folder whose children are currently being paged.
-        /// </summary>
-        public string CurrentFolderId { get; internal set; }
+		/// <summary>
+		/// Id of the folder whose children are currently being paged.
+		/// </summary>
+		public string CurrentFolderId { get; internal set; }
 
-        public override bool HasNextPage()
-        {
-            return FolderQueue.Count > 0
-                || NextPageLink != null
-                || PageRemainderBuffer.Count > 0;
-        }
-    }
+		public override bool HasNextPage()
+		{
+			return FolderQueue.Count > 0
+				|| NextPageLink != null
+				|| PageRemainderBuffer.Count > 0;
+		}
+	}
 }
