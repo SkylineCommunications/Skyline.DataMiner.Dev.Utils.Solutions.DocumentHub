3f7b76b327abfb084f7f5114592894a5bf54e395
Benjamin Hodžić <benjamin.hodzic@skyline.be>
2026-07-30 13:12:27 +0200

    Added correct reference to the SharePoint search paging context

--- DevPack/API/StorageHandlers/SharePointHandler.cs
+++ DevPack/API/StorageHandlers/SharePointHandler.cs
@@ -176,11 +176,13 @@
 			// If a filter/query is provided when ReadFiles is called, we reroute to SearchFiles.
 			if (!string.IsNullOrWhiteSpace(data.Filter))
 			{
+				var searchContext = new SharePointSearchPageData();
+				args.Context = data.Context;
 				var searchData = new WebFileSearchData
 				{
 					Bucket = data.Bucket,
 					Query = data.Filter,
-					Context = args.Context,
+					Context = searchContext,
 					Region = "EMEA", // Region is required for search requests with application-wide permissions.
 				};
 
